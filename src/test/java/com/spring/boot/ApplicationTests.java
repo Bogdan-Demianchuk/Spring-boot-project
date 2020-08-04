e3061b458fedfcc9fe94543c640b384399b248bd
Богдан Демьянчук <t0974447144@gmail.com>
2020-08-04 16:06:04 +0300

    change place for test

--- src/test/java/com/spring/boot/ApplicationTests.java
+++ src/test/java/com/spring/boot/ApplicationTests.java
@@ -6,8 +6,8 @@ import java.util.List;
 import java.util.TimeZone;
 import com.spring.boot.mapper.ReviewMapper;
 import com.spring.boot.model.dto.ReviewDto;
-import com.spring.boot.service.impl.LocalFileReaderService;
 import com.spring.boot.service.FileReaderService;
+import com.spring.boot.service.impl.LocalFileReaderService;
 import com.spring.boot.service.impl.ReviewCsvParser;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
@@ -16,8 +16,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ApplicationTests {
     FileReaderService fileRiderService = new LocalFileReaderService();
-    ReviewMapper reviewMapper = new ReviewMapper();
-    ReviewCsvParser reviewCsvParser = new ReviewCsvParser(fileRiderService, reviewMapper);
 
     @Test
     public void readCorrect() {
@@ -27,22 +25,9 @@ class ApplicationTests {
         Assert.assertEquals(expected, actual);
     }
 
-    @Test
-    void correctParseFromPathToObjects() {
-        ReviewDto reviewDto =new ReviewDto();
-        reviewDto.setProductId("B001E4KFG0");
-        reviewDto.setUserId("A3SGXH7AUHU8GW");
-        reviewDto.setProfileName("delmartian");
-        reviewDto.setHelpfulnessNominator(1L);
-        reviewDto.setHelpfulnessDenominator(1L);
-        reviewDto.setScore(5L);
-        reviewDto.setTime(LocalDateTime.ofInstant(Instant.ofEpochSecond(1303862400L),
-                TimeZone.getDefault().toZoneId()));
-        reviewDto.setSummary("Good Quality Dog Food");
-        reviewDto.setText("I have bought several of the Vitality canned dog food products and have found them all to be of good quality. The product looks more like a stew than a processed meat and it smells better. My Labrador is finicky and she appreciates this product better than  most.");
-        List<ReviewDto> expected =List.of(reviewDto);
-        List<ReviewDto> actual = reviewCsvParser.parseCsvFile("src/test/java/com/spring/boot/resources/TEST.csv");
-        Assert.assertEquals(expected, actual);
-    }
 
+
+    @Test
+    void contextLoads() {
+    }
 }
